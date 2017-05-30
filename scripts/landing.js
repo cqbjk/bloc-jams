8763044624b459e01d21d633bd3766a5641d131a
CQBJK <claudio@quabblejack.com>
2017-05-29 21:20:17 -0400

    22 scripting updates

---
+++ scripts/landing.js
@@ -0,0 +1,24 @@
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+point.style.opacity = 1;
+point.style.transform = "scaleX(1) translateY(0)";
+point.style.msTransform = "scaleX(1) translateY(0)";
+point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scroppTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
