17dac235b9d0b20953a5327f0293476666ac2319
CQBJK <claudio@quabblejack.com>
2017-05-29 20:28:36 -0400

    check 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,7 +9,7 @@ var animatePoints = function(points) {
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
 
-  for (var i=0; i < points.length; i++) {
+  for (var i=0; i < points.length; i++) { 
     revealPoint(i);
   }
 };
