cb52ba5489705aa8f07c2e08d6241cf61fabc00f
CQBJK <claudio@quabblejack.com>
2017-05-29 20:26:56 -0400

    updated checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,6 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
   var revealPoint = function(index) {
     points[index].style.opacity = 1;
@@ -12,3 +13,21 @@ var animatePoints = function() {
     revealPoint(i);
   }
 };
+
+window.onload = function() {
+
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+    if (document.documentElement.scroppTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
