c27a15a513d500823291daac668eb76a88b38759
CQBJK <claudio@quabblejack.com>
2017-06-04 18:08:32 -0400

    added 25 play + listener

--- scripts/album.js
+++ scripts/album.js
@@ -85,7 +85,7 @@ var albumMarconi = {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
-
+ 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
