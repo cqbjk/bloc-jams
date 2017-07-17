53436a48dbe7248aaa72cc4c423782f7622a599c
CQBJK <claudio@quabblejack.com>
2017-07-17 01:06:11 -0400

    committing all

--- scripts/album.js
+++ scripts/album.js
@@ -51,7 +51,7 @@ var albumMarconi = {
  var createSongRow = function(songNumber, songName, songLength) {
       var template =
          '<tr class="album-view-song-item">'
-       + '  <td class="song-item-number">' + songNumber + '</td>'
+       +       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
        + '  <td class="song-item-title">' + songName + '</td>'
        + '  <td class="song-item-duration">' + songLength + '</td>'
        + '</tr>'
@@ -86,15 +86,89 @@ var albumMarconi = {
     }
 };
 
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }
+};
+
+var clickHandler = function(targetElement) {
+};
+
+if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+       } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+           songItem.innerHTML = playButtonTemplate;
+           currentlyPlayingSong = null;
+         } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+          var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+          currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+          songItem.innerHTML = pauseButtonTemplate;
+          currentlyPlayingSong = songItem.getAttribute('data-song-number');
+      }
+
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSong = null;
 
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 
+    songListContainer.addEventListener('mouseover', function(event) {
+        // #1
+        if (event.target.parentElement.className === 'album-view-song-item') {
+                    // Change the content from the number to the play button's HTML
+                                var songItem = getSongItem(event.target);
+
+                                if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                                    songItem.innerHTML = playButtonTemplate;
+                                }
+                }
+
+               });
+
+               for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+           var songItem = getSongItem(event.target);
+           var songItemNumber = songItem.getAttribute('data-song-number');
+
+           if (songItemNumber !== currentlyPlayingSong) {
+                           songItem.innerHTML = songItemNumber;
+                       }
+         });
+
+         songRows[i].addEventListener('click', function(event) {
+                      // Event handler call
+                      clickHandler(event.target);
+
+            });
+
+     }
+
     var albums = [albumPicasso, albumMarconi, albumCher];
     var index = 1;
-    albumImage.addEventListener("click", function(event) {
+    SongRows[i].addEventListener("click", function(event) {
+      clickHandler(event.target);
       setCurrentAlbum(albums[index]);
       index++;
       if (index == albums.length) {
